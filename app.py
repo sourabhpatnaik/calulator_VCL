11910edc1d7b9f648cec004c465c24b547d0eac3
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2026-08-21 16:35:29 +0530

    Added Addition and Subtraction

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("Calculator Application")
+
+number1 = st.number_input("Enter First Number",placeholder="Example: 5")
+number2 = st.number_input("Enter Second Number",placeholder="Ex: 10")
+
+
+option = st.selectbox(
+    "Select the Operation",
+    ['Addition','Subtraction'],
+    index=None
+)
+
+res = st.button("Calculate")
+if res:
+    if option == 'Addition':
+        result = number1 + number2
+        st.write(f"Result for {option}: {result}")
+    elif option == "Subtraction":
+        result = number1 - number2
+        st.write(f"Result for {option}: {result}")
